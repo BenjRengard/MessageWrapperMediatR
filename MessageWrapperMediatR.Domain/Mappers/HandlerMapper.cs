00bf1ee11a4553ebd7428629a1ef70261a868aa3
Rengard <projectbenjaminrengard@gmail.com>
2025-10-21 11:27:18 +0200

    Add Handler Mapper

---
+++ MessageWrapperMediatR.Domain/Mappers/HandlerMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MessageWrapperMediatR.Core.Interfaces;
+using MessageWrapperMediatR.Core.Models;
+
+namespace MessageWrapperMediatR.Core.Mappers
+{
+    public class HandlerMapper : Profile
+    {
+        public HandlerMapper()
+        {
+            _ = this.CreateMap<IDynamicHandler, Handler>().ForMember(dest => dest.Bindings, opt => opt.MapFrom(src => src.Bindings)).ReverseMap();
+        }
+    }
+}
